c746a2b641fca8a0213bcc400f1b68a0217953de
D7460N <80736+dragontheory@users.noreply.github.com>
2023-05-08 20:56:49 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,8 @@
+class AppContainer extends HTMLElement {
+  constructor() {
+    super();
+    // You can add some logic here to initialize your element
+  }
+}
 
+customElements.define("app-container", AppContainer);
